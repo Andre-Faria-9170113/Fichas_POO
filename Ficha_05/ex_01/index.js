ed2cdba3c1d9ee7cd1bf96d5e2abcc3f6cf3f487
Andre-Faria-9170113 <37090430+Andre-Faria-9170113@users.noreply.github.com>
2018-04-04 00:05:04 +0100

    Add files via upload

---
+++ Ficha_05/ex_01/index.js
@@ -0,0 +1,19 @@
+//MOSTRAR RESULTADO NA ARAEA DE TEXTO ALARGADA
+let show = function (value) {
+    let myTextArea = document.querySelector("#txtResults")
+    myTextArea.innerHTML += value + "\n"
+}
+
+// ALINEA 1 #############################################
+
+class Retangulo{
+   constructor(largura=1, altura=1, cor){
+       this.largura = largura
+       this.altura = altura
+       this.cor = "branco"
+   }
+   constructor(){
+       
+
+   }
+}
--- Ficha_06/ex_02/ex02.js
+++ Ficha_06/ex_02/ex02.js
@@ -1,87 +1,65 @@
-window.onload = function(){
+window.onload = function () {
     document.getElementById("TODAS").addEventListener("click", funcTODAS)
     document.getElementById("CAMPUS I").addEventListener("click", funcC1)
     document.getElementById("CAMPUS II").addEventListener("click", funcC2)
     document.getElementById("CAMPUS III").addEventListener("click", funcC3)
 
     function funcTODAS() {
-        document.getElementById("ESMAD").style.color = "green"
-        document.getElementById("ESHT").style.color = "green"
-        document.getElementById("ESMAE").style.color = "green"
-        document.getElementById("ISEP").style.color = "green"
-        document.getElementById("ISCAP").style.color = "green"
-        document.getElementById("ESTG").style.color = "green"
-        document.getElementById("ESEP").style.color = "green"
-        document.getElementById("ESS").style.color = "green"
+        let myRef = document.getElementsByTagName("input")
 
-         document.getElementById("ESMAD").style.visibility = "visible"
-        document.getElementById("ESHT").style.visibility = "visible"
-        document.getElementById("ESMAE").style.visibility = "visible"
-        document.getElementById("ISEP").style.visibility = "visible"
-        document.getElementById("ISCAP").style.visibility ="visible"
-        document.getElementById("ESTG").style.visibility = "visible"
-        document.getElementById("ESEP").style.visibility = "visible"
-        document.getElementById("ESS").style.visibility = "visible"
+        for (let i=0; i<myRef.length; i++) {
+            myRef[i].style.backgroundColor = "green"
+            myRef[i].style.visibility = "visible"
+        }
     }
 
     function funcC1() {
-        document.getElementById("ESMAD").style.color = "black"
-        document.getElementById("ESHT").style.color = "black"
-        document.getElementById("ESMAE").style.color = "green"
-        document.getElementById("ISEP").style.color = "green"
-        document.getElementById("ISCAP").style.color = "green"
-        document.getElementById("ESTG").style.color = "black"
-        document.getElementById("ESEP").style.color = "green"
-        document.getElementById("ESS").style.color = "green"
+        let myRef = document.getElementsByTagName("input")
+        let myRef_new = document.getElementsByClassName("c1")
 
-        document.getElementById("ESMAD").style.visibility = "hidden"
-        document.getElementById("ESHT").style.visibility = "hidden"
-        document.getElementById("ESMAE").style.visibility = "visible"
-        document.getElementById("ISEP").style.visibility = "visible"
-        document.getElementById("ISCAP").style.visibility = "visible"
-        document.getElementById("ESTG").style.visibility = "hidden"
-        document.getElementById("ESEP").style.visibility = "visible"
-        document.getElementById("ESS").style.visibility = "visible"
+        for (let i=0; i<myRef.length; i++) {
+             myRef[i].style.visibility = "hidden"
+        }
+
+
+        for (let i=0; i<myRef_new.length; i++) {
+            myRef_new[i].style.visibility = "visible"
+            myRef_new[i].style.backgroundColor = "green"
+
+        }
+
     }
 
     function funcC2() {
-        document.getElementById("ESMAD").style.color = "green"
-        document.getElementById("ESHT").style.color = "green"
-        document.getElementById("ESMAE").style.color = "black"
-        document.getElementById("ISEP").style.color = "black"
-        document.getElementById("ISCAP").style.color = "black"
-        document.getElementById("ESTG").style.color = "black"
-        document.getElementById("ESEP").style.color = "black"
-        document.getElementById("ESS").style.color = "black"
+        let myRef = document.getElementsByTagName("input")
+        let myRef_new = document.getElementsByClassName("c2")
 
-        document.getElementById("ESMAD").style.visibility = "visible"
-        document.getElementById("ESHT").style.visibility = "visible"
-        document.getElementById("ESMAE").style.visibility = "hidden"
-        document.getElementById("ISEP").style.visibility = "hidden"
-        document.getElementById("ISCAP").style.visibility = "hidden"
-        document.getElementById("ESTG").style.visibility = "hidden"
-        document.getElementById("ESEP").style.visibility = "hidden"
-        document.getElementById("ESS").style.visibility = "hidden"
+        for (let i=0; i<myRef.length; i++) {
+             myRef[i].style.visibility = "hidden"
+        }
+
+
+        for (let i=0; i<myRef_new.length; i++) {
+            myRef_new[i].style.visibility = "visible"
+            myRef_new[i].style.backgroundColor = "green"
+
+        }
+
     }
 
     function funcC3() {
-        document.getElementById("ESMAD").style.color = "black"
-        document.getElementById("ESHT").style.color = "black"
-        document.getElementById("ESMAE").style.color = "black"
-        document.getElementById("ISEP").style.color = "black"
-        document.getElementById("ISCAP").style.color = "black"
-        document.getElementById("ESTG").style.color = "green"
-        document.getElementById("ESEP").style.color = "black"
-        document.getElementById("ESS").style.color = "black"
+        let myRef = document.getElementsByTagName("input")
+        let myRef_new = document.getElementsByClassName("c3")
 
-        document.getElementById("ESMAD").style.visibility = "hidden"
-        document.getElementById("ESHT").style.visibility = "hidden"
-        document.getElementById("ESMAE").style.visibility = "hidden"
-        document.getElementById("ISEP").style.visibility = "hidden"
-        document.getElementById("ISCAP").style.visibility = "hidden"
-        document.getElementById("ESTG").style.visibility = "visible"
-        document.getElementById("ESEP").style.visibility = "hidden"
-        document.getElementById("ESS").style.visibility = "hidden"
-    }
+        for (let i=0; i<myRef.length; i++) {
+             myRef[i].style.visibility = "hidden"
+        }
 
-}
+
+        for (let i=0; i<myRef_new.length; i++) {
+            myRef_new[i].style.visibility = "visible"
+            myRef_new[i].style.backgroundColor = "green"
+
+        }
+
+}}
