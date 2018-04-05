b16b2e081e9a9282ef5a576b2c2b1883f00a89a1
Andre-Faria-9170113 <37090430+Andre-Faria-9170113@users.noreply.github.com>
2018-04-05 22:47:18 +0100

    Add files via upload

--- FICHA_07/ex_02/ex02.js
+++ FICHA_07/ex_02/ex02.js
@@ -10,8 +10,16 @@ function mainFunc(){
     let dd = dataAtual.getDate()
     let mm = dataAtual.getMonth()+1
     let yyyy= dataAtual.getFullYear()
+    
 
     let frmCorrida = document.getElementById("frmCorrida")
+    let txtTelemovel = document.getElementById("txtTelemovel")
+
+    let txtCodigoPostal_01 = document.getElementById("txtCodigoPostal_01")
+    let txtCodigoPostal_02= document.getElementById("txtCodigoPostal_02")
+    let txtPassword = document.getElementById("txtPassword")
+    let txtPasswordConfirm = document.getElementById("txtPasswordConfirm")
+    let txtObservaçoes = document.getElementById("txtObservaçoes")
     
     let new_dataAtual=""
     if(mm >=10 && dd>=10){
@@ -28,18 +36,34 @@ function mainFunc(){
     }
 
     frmCorrida.addEventListener("submit", function(event){
-        var isValid = true
-
         
+        
+        if(isNaN(parseInt(txtTelemovel.value))){
+            alert("ERRO: Nº de telemóvel inválido!")
+           
+            event.preventDefault()
+
+        }
+        else if(isNaN(parseInt(txtCodigoPostal_01.value))||isNaN(parseInt(txtCodigoPostal_02.value))){
+            alert("ERRO: Nº de código postal inválido!")
+           
+            event.preventDefault()
+
+        }
+        else if(txtPassword.value != txtPasswordConfirm.value){
+            alert("ERRO: Palavras passes não correspondem!")
+           
+            event.preventDefault()
+        }
+        else{
+            alert("REGISTO EFETUADO COM SUCESSO")
+
+            
+        }
+
     })
 
     
-   
-
-
-    
-    
-    console.log(new_dataAtual)
     document.getElementById("txtNascimento").setAttribute("max", new_dataAtual)
 
     
